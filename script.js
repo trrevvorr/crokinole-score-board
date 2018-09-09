d632752a9eca530e4bd26dd2a4a15f0f1a669a03
trrevvorr <trevor.is.ross@gmail.com>
2018-09-08 19:33:40 -0500

    updated style, added favicons, removed need for jquery

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function add(points, team) {
-	$scoreEl = $(".team." + team + " .score");
-	oldScore = parseInt($scoreEl.text());
+	scoreEl = document.querySelector(".team." + team + " .score");
+	oldScore = parseInt(scoreEl.textContent);
 	newScore = oldScore + points;
-	$scoreEl.text(newScore);
+	newScore = validateScore(newScore);
+	scoreEl.textContent = newScore;
+}
+
+function validateScore(score) {
+	score = parseInt(score);
+	
+	if (isNaN(score)) {
+		score = 0;
+	}
+
+	if (score < 0) {
+		score = 0;
+	}
+
+	return score;
 }
